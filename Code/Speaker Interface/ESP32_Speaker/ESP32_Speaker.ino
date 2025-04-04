fa2c1b4a4d8271653f864b407a8cb55733c2e9d7
anthle396 <91567637+anthle396@users.noreply.github.com>
2025-04-04 13:34:33 -0700

    Update ESP32_Speaker.ino

--- Code/Speaker Interface/ESP32_Speaker/ESP32_Speaker.ino
+++ Code/Speaker Interface/ESP32_Speaker/ESP32_Speaker.ino
@@ -97,20 +97,20 @@
 // }
 
 /*
- * Happy Birthday Melody for Arduino
+ * Can't Take My Eyes Off You Melody for Arduino
  * Author: Anthony Le
  * Date: 2025-04-04
- * Description: This program plays the "Happy Birthday" song on a buzzer using predefined note frequencies.
+ * Description: This program plays a melody on a buzzer using predefined note frequencies.
  * The frequencies are based on the standard pitch notation (A4 = 440 Hz).
  * Notes are represented using an enum mapping each note to its corresponding frequency.
  */
 
-// Define an enum for note frequencies
+// Define an enum for note frequencies, avoiding pin name conflicts
 enum Note {
-  C4 = 262, C4S = 277, D4 = 294, D4S = 311, E4 = 330,
-  F4 = 349, F4S = 370, G4 = 392, G4S = 415, A4 = 440,
-  A4S = 466, B4 = 494, C5 = 523, C5S = 554, D5 = 587,
-  D5S = 622, E5 = 659, F5 = 698, F5S = 740, G5 = 784,
+  C4_ = 262, C4S_ = 277, D4_ = 294, D4S_ = 311, E4_ = 330,
+  F4_ = 349, F4S_ = 370, G4_ = 392, G4S_ = 415, A4_ = 440,
+  A4S_ = 466, B4_ = 494, C5_ = 523, C5S_ = 554, D5_ = 587,
+  D5S_ = 622, E5_ = 659, F5_ = 698, F5S_ = 740, G5_ = 784,
   NONE = 0
 };
 
@@ -125,14 +125,13 @@ void playNote(Note note, int duration) {
   delay(duration + 50);  // Wait for the note to finish and add a slight pause
 }
 
-// Function to play "Happy Birthday"
-void playHappyBirthday() {
-  // Happy Birthday melody using the Note enum
+// Function to play "Can't Take My Eyes Off You"
+void playCantTakeMyEyesOffYou() {
+  // Melody for "I love you baby and if it's quite alright I need you baby ..."
   Note melody[] = {
-    C4, NONE, C4, NONE, D4, NONE, D4, F4, E4, 
-    NONE, C4, D4, C4, G4, F4, 
-    C4, C4, C4, A4, F4, E4, 
-    D4, B4, B4, A4, F4, E4, C4
+    D4_, G4_, A4_, A4_, G4_, F4_, E4_, NONE, 
+    D4_, G4_, A4_, A4_, G4_, F4_, E4_, NONE, 
+    D4_, G4_, A4_, A4_, G4_, F4_, E4_, D4_
   };
 
   int noteDuration = 400;  // Duration for each note (in ms)
@@ -143,15 +142,75 @@ void playHappyBirthday() {
   }
 }
 
+// Function to play a user-specified note
+void playUserNote() {
+  Serial.println("Enter a note (e.g., C4_, D4_, A4_, etc.):");
+  while (!Serial.available()); // Wait for user input
+
+  String input = Serial.readStringUntil('\n');
+  input.trim(); // Remove any whitespace or newlines
+
+  Note selectedNote = NONE;
+
+  // Use switch statement for note mapping
+  if (input.length() > 0) {
+    switch (input[0]) {
+      case 'C':
+        if (input == "C4_") selectedNote = C4_;
+        else if (input == "C4S_") selectedNote = C4S_;
+        else if (input == "C5_") selectedNote = C5_;
+        else if (input == "C5S_") selectedNote = C5S_;
+        break;
+      case 'D':
+        if (input == "D4_") selectedNote = D4_;
+        else if (input == "D4S_") selectedNote = D4S_;
+        else if (input == "D5_") selectedNote = D5_;
+        else if (input == "D5S_") selectedNote = D5S_;
+        break;
+      case 'E':
+        if (input == "E4_") selectedNote = E4_;
+        else if (input == "E5_") selectedNote = E5_;
+        break;
+      case 'F':
+        if (input == "F4_") selectedNote = F4_;
+        else if (input == "F4S_") selectedNote = F4S_;
+        else if (input == "F5_") selectedNote = F5_;
+        else if (input == "F5S_") selectedNote = F5S_;
+        break;
+      case 'G':
+        if (input == "G4_") selectedNote = G4_;
+        else if (input == "G4S_") selectedNote = G4S_;
+        else if (input == "G5_") selectedNote = G5_;
+        break;
+      case 'A':
+        if (input == "A4_") selectedNote = A4_;
+        else if (input == "A4S_") selectedNote = A4S_;
+        break;
+      case 'B':
+        if (input == "B4_") selectedNote = B4_;
+        break;
+    }
+  }
+
+  if (selectedNote != NONE) {
+    Serial.println("Playing: " + input);
+    playNote(selectedNote, 500);
+  } else {
+    Serial.println("Invalid note. Try again.");
+  }
+}
+
 void setup() {
   pinMode(speakerPin, OUTPUT);
   Serial.begin(115200);
-  Serial.println("Playing Happy Birthday!");
+  Serial.println("Playing 'Can't Take My Eyes Off You'!");
 }
 
 void loop() {
-  playHappyBirthday();
+  playCantTakeMyEyesOffYou();
   delay(10000);  // Pause before replaying the song
+  playUserNote(); // Allow user to input a note
 }
+
 
 
